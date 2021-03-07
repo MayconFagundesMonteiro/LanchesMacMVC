b7ce01c080d83a24b09f24a565c73118fccc79b7
Maycon Fagundes <mayconfagundes0933@gmail.com>
2021-03-07 17:17:42 -0300

    Add Controlador e View para Exibir Lanches, com os padrões de VViewModels, e Partial View

---
+++ LanchesMacMVC/Controllers/LancheController.cs
@@ -0,0 +1,32 @@
+﻿using LanchesMacMVC.Models.ViewModels;
+using LanchesMacMVC.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMacMVC.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaRepository categoriaRepository )
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+        public IActionResult List()
+        {
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.Lanches = _lancheRepository.Lanches;
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lancheListViewModel);
+        }
+    }
+}
---
+++ LanchesMacMVC/Models/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMacMVC.Models.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<Lanche> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
